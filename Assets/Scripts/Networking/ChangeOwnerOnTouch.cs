2c1c65a6692466756ca5616b850d70d4cb1f667e
Rimme <rimme@happyvolcano.com>
2026-08-28 21:29:49 +0200

    Change ownership thingy

--- Assets/Scripts/Networking/ChangeOwnerOnTouch.cs
+++ Assets/Scripts/Networking/ChangeOwnerOnTouch.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class ChangeOwnerOnTouch : NetworkBehaviour
 {
+    protected override void OnNetworkPostSpawn()
+    {
+        if (!IsServer)
+        {
+            Destroy(this);
+        }
+    }
+
     private void OnCollisionEnter(Collision other)
     {
-        if (!IsOwner)
-            return;
-        
-        if(other.gameObject.TryGetComponent(out NetworkObject networkObject))
+        if (other.rigidbody.TryGetComponent(out NetworkObject networkObject))
+        {
             NetworkObject.ChangeOwnership(networkObject.OwnerClientId);
+            Debug.Log($"Changed owner to: {networkObject.OwnerClientId}");
+        }
     }
 }
