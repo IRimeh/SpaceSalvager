62383aff08f5a6b5e442329db3409a96c67d540a
Rimme <rimme@happyvolcano.com>
2026-08-30 00:22:21 +0200

    few small things

--- Assets/Features/Interactables/Interactable.cs
+++ Assets/Features/Interactables/Interactable.cs
@@ -4,4 +4,10 @@ public class Interactable : NetworkBehaviour
 {
 	public NetworkVariable<bool> IsBeingHeld = new(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 	public bool IsBeingHeldByLocalPlayer = false;
+
+	[Rpc(SendTo.Owner)]
+	public void SetIsBeingHeldRpc(bool isBeingHeld)
+	{
+		IsBeingHeld.Value = isBeingHeld;
+	}
 }
