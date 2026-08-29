b8c68426d4dfd7cefd1801e11703772103558e6f
Rimme <rimme@happyvolcano.com>
2026-08-29 16:13:05 +0200

    Changed isbeingheldlocal to normal bool

--- Assets/Features/Interactables/Interactable.cs
+++ Assets/Features/Interactables/Interactable.cs
@@ -1,8 +1,7 @@
 using Unity.Netcode;
-using UnityEngine;
 
 public class Interactable : NetworkBehaviour
 {
 	public NetworkVariable<bool> IsBeingHeld = new(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-	public NetworkVariable<bool> IsBeingHeldByLocalPlayer = new(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+	public bool IsBeingHeldByLocalPlayer = false;
 }
