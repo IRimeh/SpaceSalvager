6e34e6eff19c72b16a49f3ed0502ef6b896016f0
Daniel Henseler <danielhenseler@badcodes.de>
2026-08-30 16:10:26 +0200

    Gun effect fix

--- Assets/Scripts/ToolGravitygun.cs
+++ Assets/Scripts/ToolGravitygun.cs
@@ -34,7 +34,7 @@ public class ToolGravitygun : Tool
 	public float CurrentCharge01 => Mathf.Clamp01(currentCharge / timeToMaxCharge);
 	public event Action<float> OnShootEvent = delegate { };
 
-	protected override void OnNetworkPostSpawn() 
+	protected override void OnNetworkPostSpawn()
 	{
 		playerCamera = transform.parent.parent;
 	}
@@ -57,28 +57,30 @@ public class ToolGravitygun : Tool
 
 	public override void ReleaseSecondary()
 	{
+		if (!IsOwner) return;
+
 		isCharging = false;
 
 		float PushForce = ((maxPushforce - minPushforce) * CurrentCharge01) + minPushforce;
 
 		RaycastHit[] hits = Physics.SphereCastAll(playerCamera.position, spherecastRadius, playerCamera.forward, maxPushDistance);
 
-		foreach (RaycastHit hit in hits) 
+		foreach (RaycastHit hit in hits)
 		{
-			if (hit.rigidbody != null && hit.transform.gameObject != this.NetworkObject.transform.gameObject) 
+			if (hit.rigidbody != null && hit.transform.gameObject != this.NetworkObject.transform.gameObject)
 			{
 				Vector3 EvaluatedPushForce = playerCamera.forward * PushForce * pushFalloff.Evaluate(NormalizeAndClamp(hit.distance, 0.0f, maxPushDistance));
 				Debug.Log(hit.distance + " / " + " / " + NormalizeAndClamp(hit.distance, 0.0f, maxPushDistance) + " / " + pushFalloff.Evaluate(NormalizeAndClamp(hit.distance, 0.0f, maxPushDistance)) + " / " + EvaluatedPushForce.magnitude);
 
-					if (hit.transform.TryGetComponent<NetworkObject>(out NetworkObject targetObjects))
-						ApplyPushForceServerRpc(targetObjects.NetworkObjectId, EvaluatedPushForce, hit.point);
+				if (hit.transform.TryGetComponent<NetworkObject>(out NetworkObject targetObjects))
+					ApplyPushForceServerRpc(targetObjects.NetworkObjectId, EvaluatedPushForce, hit.point);
 			}
 
 			if (hit.transform.TryGetComponent(out InteractableView interactableView))
 				interactableView.ShowEffectForTime(5.0f);
 		}
 
-		OnShootEvent.Invoke(CurrentCharge01);
+		PlaySecondaryShootEffectEveryoneRpc();
 		currentCharge = 0;
 	}
 
@@ -97,6 +99,12 @@ public class ToolGravitygun : Tool
 
 		float normalized = (value - min) / (max - min);
 		return Mathf.Clamp(normalized, 0f, 1f);
+	}
+
+	[Rpc(SendTo.Everyone)]
+	private void PlaySecondaryShootEffectEveryoneRpc()
+	{
+		OnShootEvent.Invoke(CurrentCharge01);
 	}
 
 	[Rpc(SendTo.Server)]
