6b90f43ecfde8892517318c736d79c68929d1215
Rimme <rimme@happyvolcano.com>
2026-08-31 19:08:01 +0200

    More huge UI improvements

--- Assets/Features/Interactables/InteractableView.cs
+++ Assets/Features/Interactables/InteractableView.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using DG.Tweening;
@@ -8,13 +7,12 @@ using UnityEngine;
 public class InteractableView : MonoBehaviour
 {
 	[SerializeField] private Interactable _interactable;
-	[SerializeField] private MeshRenderer _meshRenderer;
+	[SerializeField] private MeshFilter _sourceMeshFilter;
 	[SerializeField] private MeshFilter _targetFilter;
+	[SerializeField] private List<MeshRenderer> _outlineMeshRenderers;
 	[SerializeField] private ParticleSystem _particleSystem;
-	[SerializeField] private List<MeshFilter> _meshFilters;
 	[SerializeField] private int _outlineLayerIndex = 1;
 	[SerializeField] private float _scaleSpeed = 0.25f;
-	[SerializeField] private float _effectOnCollisionTime = 1.0f;
 
 	private float _currentTime = 0;
 	
@@ -35,6 +33,32 @@ public class InteractableView : MonoBehaviour
 		_interactable.IsBeingHeld.OnValueChanged -= OnIsBeingHeldChanged;
 	}
 
+	private void OnIsBeingHeldChanged(bool previousvalue, bool isHeld)
+	{
+		SetOutlineOnMeshRenderers(isHeld);
+		if (isHeld)
+		{
+			_particleSystem.Play();
+			_targetFilter.transform.DOScale(Vector3.one, _scaleSpeed);
+		}
+		else
+		{
+			_particleSystem.Stop();
+			_targetFilter.transform.DOScale(Vector3.zero, _scaleSpeed);
+		}
+	}
+
+	private void SetOutlineOnMeshRenderers(bool show)
+	{
+		foreach (MeshRenderer meshRenderer in _outlineMeshRenderers)
+		{
+			if(show)
+				meshRenderer.renderingLayerMask |= 1u << _outlineLayerIndex;
+			else
+				meshRenderer.renderingLayerMask &= ~(1u << _outlineLayerIndex);
+		}
+	}
+	
 	private void Update()
 	{
 		if (_currentTime <= 0)
@@ -47,34 +71,16 @@ public class InteractableView : MonoBehaviour
 
 	public void ShowEffectForTime(float time)
 	{
+		if (_interactable.IsBeingHeld.Value)
+			return;
+		
 		_currentTime = time;
 		_interactable.SetIsBeingHeldServerRpc(true);
 	}
 
-	private void OnCollisionEnter(Collision other)
-	{
-		ShowEffectForTime(_effectOnCollisionTime);
-	}
-
-	private void OnIsBeingHeldChanged(bool previousvalue, bool isHeld)
-	{
-		if (isHeld)
-		{
-			_meshRenderer.renderingLayerMask |= 1u << _outlineLayerIndex;
-			_particleSystem.Play();
-			_targetFilter.transform.DOScale(Vector3.one, _scaleSpeed);
-		}
-		else
-		{
-			_meshRenderer.renderingLayerMask &= ~(1u << _outlineLayerIndex);
-			_particleSystem.Stop();
-			_targetFilter.transform.DOScale(Vector3.zero, _scaleSpeed);
-		}
-	}
-
 	private void CopyMeshes()
 	{
-		Mesh mesh = Instantiate(_meshFilters[0].sharedMesh);
+		Mesh mesh = Instantiate(_sourceMeshFilter.sharedMesh);
 		List<Vector3> verts = mesh.vertices.ToList();
 		List<Vector2> uvs = mesh.uv.ToList();
 		List<int> triangles = mesh.triangles.ToList();
@@ -112,7 +118,7 @@ public class InteractableView : MonoBehaviour
 
 		for (int i = 0; i < triangles.Count; i++)
 		{
-			if (uniqueVertIndexPointer.TryGetValue(_meshFilters[0].sharedMesh.vertices[triangles[i]], out int newIndex))
+			if (uniqueVertIndexPointer.TryGetValue(_sourceMeshFilter.sharedMesh.vertices[triangles[i]], out int newIndex))
 				triangles[i] = newIndex;
 		}
 
